d270aacac0dd6e1a0259f88d79a6052d6c39e86c
Alejandro Hernández Rodríguez <roher1727@gmail.com>
2024-07-24 11:46:19 -0500

    Create ping.js

---
+++ ping.js
@@ -0,0 +1,13 @@
+const https = require('https');
+
+const pingUrl = 'https://example.com'; // Replace with the URL you want to ping
+
+function pingWebsite() {
+  https.get(pingUrl, (res) => {
+    console.log(`Pinged ${pingUrl} - Status Code: ${res.statusCode}`);
+  }).on('error', (err) => {
+    console.error(`Error pinging ${pingUrl}: ${err.message}`);
+  });
+}
+
+pingWebsite();
