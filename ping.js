5995cfa7afc6669abf5e86ef2bce8a3e4cde1752
Alejandro Hernández Rodríguez <roher1727@gmail.com>
2024-07-27 21:24:02 -0500

    Update ping.js

--- ping.js
+++ ping.js
@@ -1,4 +1,5 @@
 const https = require('https');
+const cron = require('node-cron');
 
 const pingUrl = 'https://hisaul.com'; // Replace with the URL you want to ping
 
@@ -10,4 +11,11 @@ function pingWebsite() {
   });
 }
 
-pingWebsite();
+// Schedule the task to run every 5 minutes
+cron.schedule('*/5 * * * *', () => {
+  console.log('Running task every 5 minutes');
+  pingWebsite();
+});
+
+// Keep the process running
+setInterval(() => {}, 1 << 30);
